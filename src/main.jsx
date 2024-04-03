0c1a9e3f643d6a9de28c5040816c6dd360341e8b
Jhonatan Castillo Sullca <jcastillosullca@gmail.com>
2024-04-02 20:40:53 -0500

    install i18

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './pages/App/index.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import i18next from 'i18next'
+import { initReactI18next } from 'react-i18next'
+
+i18next.use(initReactI18next).init({
+  lng: "es",
+  interpolation: {
+    escapeValue: false,
+  },
+  resources: {
+    es: {
+      translation: "",
+    },
+    en: {
+      translation: "",
+    }
+  }
+
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
